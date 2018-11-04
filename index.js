a0f4ad73e6169ac304d4c4e7c19f9248eaf40b6b
Adrianne Padua <admarpad@gmail.com>
2018-11-04 02:32:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 // write your code below!
-function happyHolidays(string) {
-  return "Happy holidays!"
+function happyHolidays() {
+  return 'Happy holidays!'
 }
-function happyHolidaysTo(name) {
-  return `Happy Holidays, ${name}!`
-}
-happyHolidaysTo(holiday, name) {
-  console.log(`Happy ${holiday}, ${name}!`)
-}
-function holidayCountdown(days, holiday) {
-  console.log(`It's ${days} until ${holiday}`)
-}
+
+
